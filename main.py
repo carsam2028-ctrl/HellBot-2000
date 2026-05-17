e19415870315f0d52ac7616baffe52c0f02faa17
Sami SZ <sebastiansz2028@gmail.com>
2026-05-16 21:06:58 -0400

    Added error handler, added a base for a mute command

--- main.py
+++ main.py
@@ -19,13 +19,43 @@ class HellBot(commands.Bot):
         print(signature_print + f'Logged on as {self.user}!')
         await self.tree.sync()
         print(signature_print + 'Synced Commands Globally!')
-        await self.tree.sync(guild=discord.Object(id=1504288510650093570))
+        await self.tree.sync(guild=discord.Object(id=1442700064136101908))
         print(signature_print + 'Synced Commands in Home Server!')
-        await bot.change_presence(activity=discord.Game(name="In Development"))
+        await bot.change_presence(activity=discord.Game(name="For Super Earth!!"))
         print(signature_print + f"{bot.user.name} presence set!")
 
-
 bot = HellBot(command_prefix='CB!', intents=intents)
+#Error Handler
+@bot.tree.error
+async def on_app_command_error(interaction: discord.Interaction, error):
+    err_msg = ""
+    print(signature_print + f"Error: {str(error)}")
+    if isinstance(error, app_commands.CommandInvokeError):
+        error = error.original
+        err_msg = "An error has occurred, please try again."
+    elif isinstance(error, app_commands.MissingPermissions):
+        err_msg = "You do not have the necessary permission(s) for this command."
+    elif isinstance(error, app_commands.BotMissingPermissions):
+        err_msg = "Bot does not have required permission(s)."
+    elif isinstance(error, discord.HTTPException):
+        err_msg = "An HTTP Exception has occurred, try again."
+    elif isinstance(error, discord.HTTPException) and error.status == 429:
+        err_msg = "We are being rate limited, please try again after a couple seconds."
+    else:
+        err_msg = f"A fatal error has occurred: {str(error)}"
 
-#Loop
+
+    if interaction.response.is_done():
+        await interaction.followup.send(f"{err_msg}", ephemeral=True)
+    else:
+        await interaction.response.send_message(f"{err_msg}", ephemeral=True)
+
+
+#Commands
+@bot.tree.command(name="mute", description="Mutes selected user")
+@app_commands.checks.has_permissions(mute_members=True)
+async def mute(interaction: discord.Interaction, member: discord.Member):
+    await interaction.response.defer()
+
+#Bot Startup P2
 bot.run(token=DISCORD_TOKEN)
