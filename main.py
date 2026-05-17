47cf6abf7fd955bb63aea21b4ffcdb35f0e4de22
Sami SZ <sebastiansz2028@gmail.com>
2026-05-16 23:19:27 -0400

    Added a fun super earth message random, adjusted timeout and untimeout commands

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 #Imports
 import os
+import random
+
 import discord
-import dotenv
 from dotenv import load_dotenv
 from discord.ext import commands
 from discord import app_commands
+from datetime import timedelta
 load_dotenv()
 
 #Variables
@@ -41,6 +43,9 @@ async def on_app_command_error(interaction: discord.Interaction, error):
         err_msg = "An HTTP Exception has occurred, try again."
     elif isinstance(error, discord.HTTPException) and error.status == 429:
         err_msg = "We are being rate limited, please try again after a couple seconds."
+    elif isinstance(error, discord.Forbidden):
+        error = error.original
+        err_msg = "Bot missing permissions."
     else:
         err_msg = f"A fatal error has occurred: {str(error)}"
 
@@ -52,10 +57,55 @@ async def on_app_command_error(interaction: discord.Interaction, error):
 
 
 #Commands
-@bot.tree.command(name="mute", description="Mutes selected user")
-@app_commands.checks.has_permissions(mute_members=True)
-async def mute(interaction: discord.Interaction, member: discord.Member):
+@bot.tree.command(name="timeout", description="Timeouts selected user")
+@app_commands.checks.has_permissions(mute_members=True, moderate_members=True)
+@app_commands.describe(time="Time (in minutes) you want to timeout this person.")
+@app_commands.describe(member="Person you want to timeout.")
+@app_commands.describe(reason="Reason to timeout this user.")
+async def mute(interaction: discord.Interaction, member: discord.Member, time: int, reason: str):
+    await interaction.response.defer(ephemeral=False)
+    duration = timedelta(minutes=time)
+    if member == interaction.user:
+        await interaction.followup.send("You cannot timeout yourself!", ephemeral=True)
+    if time < 1:
+        await interaction.followup.send("Time cannot be less than 1 minute.", ephemeral=True)
+    if member == bot.user:
+        await interaction.followup.send("You cannot timeout the bot this way.", ephemeral=True)
+    await member.timeout(duration, reason=reason)
+    await interaction.followup.send(f"{member} has been timed out for {time} minutes. Reason: {reason}", ephemeral=False)
+
+@bot.tree.command(name="untimeout", description="Un-time outs selected user")
+@app_commands.checks.has_permissions(mute_members=True, moderate_members=True)
+@app_commands.describe(reason="Reason to unmute this user.")
+async def mute(interaction: discord.Interaction, member: discord.Member, reason: str):
+    await interaction.response.defer(ephemeral=False)
+    duration = None
+    if member == interaction.user:
+        await interaction.followup.send("You cannot un-time out yourself!", ephemeral=True)
+    if not member.is_timed_out():
+        await interaction.followup.send("User is not timed out!")
+    await member.timeout(duration, reason=reason)
+    await interaction.followup.send(f"{member} has been untimed out. Reason: {reason}", ephemeral=False)
+
+@bot.tree.command(name="semftd", description="Super Earth Messages for the day")
+async def semftd(interaction: discord.Interaction):
     await interaction.response.defer()
+    semftd1 = "Super Earth liberates those from tyranny and deception!"
+    semftd2 = "Automatons are lead under tyranny and control."
+    semftd3 = "Helldivers protect our liberty and Managed Democracy!"
+    semftd4 = "Squids, Bugs, and Automatons destroy our way of life, stop them!"
+    semftd5 = "Report any undemocratic speech to your democracy officer!"
+    random_semftd = random.randint(1, 5)
+    if random_semftd == 1:
+        await interaction.followup.send(semftd1)
+    if random_semftd == 2:
+        await interaction.followup.send(semftd2)
+    if random_semftd == 3:
+        await interaction.followup.send(semftd3)
+    if random_semftd == 4:
+        await interaction.followup.send(semftd4)
+    if random_semftd == 5:
+        await interaction.followup.send(semftd5)
 
 #Bot Startup P2
 bot.run(token=DISCORD_TOKEN)
